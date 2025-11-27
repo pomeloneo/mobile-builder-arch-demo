31801e9768d4defe285baa0160f6dfc348e28784
neo <pomelo.neo@qq.com>
2025-11-27 22:42:28 +0800

    rm code

--- packages/h5-builder/src/flow/index.ts
+++ packages/h5-builder/src/flow/index.ts
@@ -1,5 +1,1 @@
-// ComponentLoader has been moved to services/component.service.ts and renamed to ComponentService
-export { ComponentRegistry } from './component-loader';
-export type { ComponentSchema } from './component-loader';
-
 export { ErrorPlaceholderModel, LoadingPlaceholderModel, EmptyPlaceholderModel } from './placeholders';
