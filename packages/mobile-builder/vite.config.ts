994f6e777581f17f515b978019e38b2e64b3b56a
limantang.neo <limantang.neo@bytedance.com>
2025-12-03 20:17:37 +0800

    添加构建

--- packages/mobile-builder/vite.config.ts
+++ packages/mobile-builder/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
@@ -6,7 +6,6 @@ import path from 'path';
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), '');
   const CDN_URL = "https://cdn-tos-cn.bytedance.net/obj/archi/caravel-lynx-demo/"
 
   return {
@@ -14,7 +13,7 @@ export default defineConfig(({ mode }) => {
 
     // Base public path - adjust for deployment
     // Use CDN_URL environment variable if available, otherwise fallback to relative path
-    base: `https://${env.CDN_OUTER_SG}` || './',
+    base: CDN_URL || './',
 
     resolve: {
       alias: {
